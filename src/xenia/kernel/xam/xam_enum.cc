74ef0299416cd24fe4855b065ff89681623f48f6
Codie Stella <git@codie.gg>
2023-03-10 17:51:26 +0000

    Quick bug fixes.

--- src/xenia/kernel/xam/xam_enum.cc
+++ src/xenia/kernel/xam/xam_enum.cc
@@ -201,6 +201,8 @@ dword_result_t XamCreateEnumeratorHandle_entry(
           memcpy(item->server_description, server["description"].GetString(),
                  strlen(server["description"].GetString()));
         }
+      } else {
+          return 1;
       }
 
 #pragma endregion
--- src/xenia/kernel/xam/xam_net.h
+++ src/xenia/kernel/xam/xam_net.h
@@ -28,6 +28,8 @@ uint16_t GetMappedConnectPort(uint16_t port);
 uint16_t GetMappedBindPort(uint16_t port);
 std::string GetApiAddress();
 
+void resetQosCache();
+
 }  // namespace xam
 }  // namespace kernel
 }  // namespace xe
