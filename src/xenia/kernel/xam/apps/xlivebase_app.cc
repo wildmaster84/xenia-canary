e833f2e0120b7d3b0a1c3ee3c62a496762a49db9
Radosław Gliński <Gliniak93@gmail.com>
2023-12-02 22:14:18 +0100

    [Xlivebase] Added partial handling for creating friends enumerator (#5)

--- src/xenia/kernel/xam/apps/xlivebase_app.cc
+++ src/xenia/kernel/xam/apps/xlivebase_app.cc
@@ -8,9 +8,11 @@
  */
 
 #include "xenia/kernel/xam/apps/xlivebase_app.h"
+#include "xenia/kernel/xenumerator.h"
 
 #include "xenia/base/logging.h"
 #include "xenia/base/threading.h"
+#include "xenia/kernel/util/shim_utils.h"
 
 #ifdef XE_PLATFORM_WIN32
 // NOTE: must be included last as it expects windows.h to already be included.
@@ -27,6 +29,21 @@ namespace kernel {
 namespace xam {
 namespace apps {
 
+// TODO(Gliniak): Find better names for these structures!
+struct X_ARGUEMENT_ENTRY {
+  xe::be<uint32_t> magic_number;
+  xe::be<uint32_t> unk_1;
+  xe::be<uint32_t> unk_2;
+  xe::be<uint32_t> object_ptr;
+};
+static_assert_size(X_ARGUEMENT_ENTRY, 0x10);
+
+struct X_ARGUMENT_LIST {
+  X_ARGUEMENT_ENTRY entry[32];
+  xe::be<uint32_t> argument_count;
+};
+static_assert_size(X_ARGUMENT_LIST, 0x204);
+
 XLiveBaseApp::XLiveBaseApp(KernelState* kernel_state)
     : App(kernel_state, 0xFC) {}
 
@@ -37,6 +54,7 @@ X_HRESULT XLiveBaseApp::DispatchMessageSync(uint32_t message,
                                             uint32_t buffer_length) {
   // NOTE: buffer_length may be zero or valid.
   auto buffer = memory_->TranslateVirtual(buffer_ptr);
+
   switch (message) {
     case 0x00058004: {
       // Called on startup, seems to just return a bool in the buffer.
@@ -86,9 +104,9 @@ X_HRESULT XLiveBaseApp::DispatchMessageSync(uint32_t message,
       // We should create a XamEnumerate-able empty list here, but I'm not
       // sure of the format.
       // buffer_length seems to be the same ptr sent to 0x00058004.
-      XELOGD("CXLiveFriends::Enumerate({:08X}, {:08X}) unimplemented",
-             buffer_ptr, buffer_length);
-      return X_E_FAIL;
+      XELOGD("CXLiveFriends::Enumerate({:08X}, {:08X})", buffer_ptr,
+             buffer_length);
+      return CreateFriendsEnumerator(buffer_length);
     }
     case 0x00058023: {
       XELOGD(
@@ -137,6 +155,60 @@ X_HRESULT XLiveBaseApp::DispatchMessageSync(uint32_t message,
       "arg2={:08X}",
       app_id(), message, buffer_ptr, buffer_length);
   return X_E_FAIL;
+}
+
+X_HRESULT XLiveBaseApp::CreateFriendsEnumerator(uint32_t buffer_args) {
+  if (!buffer_args) {
+    return X_E_INVALIDARG;
+  }
+
+  Memory* memory = kernel_state_->memory();
+
+  X_ARGUMENT_LIST* arg_list =
+      memory->TranslateVirtual<X_ARGUMENT_LIST*>(buffer_args);
+
+  if (arg_list->argument_count <= 3) {
+    assert_always(
+        "XLiveBaseApp::CreateFriendsEnumerator - Invalid argument count!");
+  }
+
+  const uint32_t user_index = xe::load_and_swap<uint32_t>(
+      memory->TranslateVirtual(arg_list->entry[0].object_ptr));
+  const uint32_t friends_starting_index = xe::load_and_swap<uint32_t>(
+      memory->TranslateVirtual(arg_list->entry[1].object_ptr));
+  const uint32_t friends_amount = xe::load_and_swap<uint32_t>(
+      memory->TranslateVirtual(arg_list->entry[2].object_ptr));
+
+  if (friends_starting_index >= 0x64) {
+    return X_E_INVALIDARG;
+  }
+
+  if (friends_amount > 0x64) {
+    return X_E_INVALIDARG;
+  }
+
+  const uint32_t buffer_address = arg_list->entry[3].object_ptr;
+  const uint32_t handle_address = arg_list->entry[4].object_ptr;
+
+  if (!buffer_address) {
+    return X_E_INVALIDARG;
+  }
+
+  if (!handle_address) {
+    return X_E_INVALIDARG;
+  }
+
+  uint32_t* buffer_ptr = memory->TranslateVirtual<uint32_t*>(buffer_address);
+  uint32_t* handle_ptr = memory->TranslateVirtual<uint32_t*>(handle_address);
+
+  // TODO(Gliniak): Enumerator itself stores user_index XUID at enumerator
+  // address + 0x24
+  auto e =
+      make_object<XStaticUntypedEnumerator>(kernel_state_, friends_amount, 0);
+  auto result = e->Initialize(-1, app_id(), 0x58021, 0x58022, 0, 0x10, nullptr);
+
+  *handle_ptr = (uint32_t)e->handle();
+  return X_E_SUCCESS;
 }
 
 }  // namespace apps
