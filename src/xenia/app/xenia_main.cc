605f61666fc23ed3ce8cecd1e858266c73d46a79
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2023-12-24 16:29:52 +0000

    [XLiveAPI] Added DeleteSessionsByMac endpoint
    Delete sessions by mac when host or guest crashes.

--- src/xenia/app/xenia_main.cc
+++ src/xenia/app/xenia_main.cc
@@ -524,7 +524,7 @@ void EmulatorApp::OnDestroy() {
   }
 
   // Delete sessions on shutdown.
-  xe::kernel::XLiveAPI::DeleteAllSessions();
+  xe::kernel::XLiveAPI::DeleteAllSessionsByMac();
 
   curl_global_cleanup();
 #pragma endregion
