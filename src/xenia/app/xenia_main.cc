427b22d59307b4f30987faa06f4ae17dfac24f64
Gliniak <Gliniak93@gmail.com>
2024-01-21 11:55:24 +0100

    [UPnP] UPnP Cleanup

--- src/xenia/app/xenia_main.cc
+++ src/xenia/app/xenia_main.cc
@@ -506,7 +506,7 @@ void EmulatorApp::OnDestroy() {
 #pragma region NetplayCleanup
   // UPnP Shutdown
   if (cvars::upnp) {
-    xe::kernel::XLiveAPI::upnp_handler.~upnp();
+    delete xe::kernel::XLiveAPI::upnp_handler;
   }
 
   // Delete sessions on shutdown.
