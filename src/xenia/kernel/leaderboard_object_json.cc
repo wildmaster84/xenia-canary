a262a52aea63841941a8abb17a245ce4e1ad9e72
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-02-14 23:25:58 +0000

    [XLiveAPI] JSON objects for serialization and deserialization

---
+++ src/xenia/kernel/leaderboard_object_json.cc
@@ -0,0 +1,97 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2024 Xenia Emulator. All rights reserved.                        *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#include "xenia/kernel/leaderboard_object_json.h"
+#include "xenia/kernel/util/shim_utils.h"
+
+namespace xe {
+namespace kernel {
+LeaderboardObjectJSON::LeaderboardObjectJSON() : stats_{}, view_properties_{} {}
+
+LeaderboardObjectJSON::~LeaderboardObjectJSON() {}
+
+bool LeaderboardObjectJSON::Deserialize(const rapidjson::Value& obj) {
+  return true;
+}
+
+bool LeaderboardObjectJSON::Serialize(
+    rapidjson::PrettyWriter<rapidjson::StringBuffer>* writer) const {
+  const std::string xuid =
+      fmt::format("{:016X}", static_cast<uint64_t>(stats_.xuid));
+
+  writer->StartObject();
+
+  writer->Key("leaderboards");
+  writer->StartObject();
+
+  for (auto& user_property : view_properties_) {
+    const std::string leaderboard_id =
+        std::to_string(user_property.leaderboard_id);
+
+    writer->Key(leaderboard_id);
+    writer->StartObject();
+
+    writer->Key("stats");
+    writer->StartObject();
+
+    for (uint32_t i = 0; i < user_property.properties_count; i++) {
+      const XUSER_PROPERTY* statistics_ptr =
+          kernel_state()->memory()->TranslateVirtual<XUSER_PROPERTY*>(
+              user_property.properties_guest_address);
+
+      const XUSER_PROPERTY& stat = statistics_ptr[i];
+
+      const std::string property_id =
+          fmt::format("{:08X}", static_cast<uint32_t>(stat.property_id));
+
+      // Write each stat ID
+      writer->Key(property_id);
+      writer->StartObject();
+
+      writer->Key("type");
+      writer->Int(static_cast<uint32_t>(stat.data.type));
+
+      switch (stat.data.type) {
+        case X_USER_DATA_TYPE::INT32: {
+          writer->String("value");
+          writer->Uint(stat.data.s32);
+        } break;
+        case X_USER_DATA_TYPE::INT64: {
+          writer->String("value");
+          writer->Uint64(stat.data.s64);
+        } break;
+        case X_USER_DATA_TYPE::DOUBLE: {
+          writer->String("value");
+          writer->Double(stat.data.f64);
+        } break;
+        default:
+          XELOGW("Unimplemented statistic type for write {}",
+                 static_cast<uint32_t>(stat.data.type));
+          break;
+      }
+
+      writer->EndObject();
+    }
+
+    writer->EndObject();
+
+    writer->EndObject();
+  }
+
+  writer->EndObject();
+
+  writer->Key("xuid");
+  writer->String(xuid);
+
+  writer->EndObject();
+
+  return true;
+}
+}  // namespace kernel
+}  // namespace xe
---
+++ src/xenia/kernel/leaderboard_object_json.h
@@ -0,0 +1,40 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2024 Xenia Emulator. All rights reserved.                        *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#include "xenia//kernel/xsession.h"
+#include "xenia/kernel/base_object_json.h"
+
+namespace xe {
+namespace kernel {
+class LeaderboardObjectJSON : public BaseObjectJSON {
+ public:
+  LeaderboardObjectJSON();
+  virtual ~LeaderboardObjectJSON();
+
+  virtual bool Deserialize(const rapidjson::Value& obj);
+  virtual bool Serialize(
+      rapidjson::PrettyWriter<rapidjson::StringBuffer>* writer) const;
+
+  const std::vector<XSessionViewProperties>& ViewProperties() const {
+    return view_properties_;
+  }
+  void ViewProperties(
+      const std::vector<XSessionViewProperties>& view_properties) {
+    view_properties_ = view_properties;
+  }
+
+  const XSessionWriteStats& Stats() const { return stats_; }
+  void Stats(const XSessionWriteStats& stats) { stats_ = stats; }
+
+ private:
+  XSessionWriteStats stats_;
+  std::vector<XSessionViewProperties> view_properties_;
+};
+}  // namespace kernel
+}  // namespace xe
