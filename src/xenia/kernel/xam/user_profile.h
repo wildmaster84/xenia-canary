dbefa6d7752e1afa254bb08eb118145d31ada482
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-09-19 18:24:23 +0100

    [XAM] Revert type check in XamUserGetXUID

--- src/xenia/kernel/xam/user_profile.h
+++ src/xenia/kernel/xam/user_profile.h
@@ -111,6 +111,10 @@ class UserProfile {
     return cvars::offline_mode ? X_USER_SIGNIN_STATE::SignedInLocally
                                : X_USER_SIGNIN_STATE::SignedInToLive;
   }
+  uint32_t type() const {
+    return static_cast<uint32_t>(X_USER_SIGNIN_STATE::SignedInLocally) |
+           static_cast<uint32_t>(X_USER_SIGNIN_STATE::SignedInToLive);
+  }
 
   uint32_t GetReservedFlags() const {
     return account_info_.GetReservedFlags();
