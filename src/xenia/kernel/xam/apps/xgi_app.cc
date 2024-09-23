ac0fa625e126373b6a3ea76c594cb7fdeeb81735
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-09-23 19:22:50 +0100

    [XGI] Log statistic type in XUserReadStats

--- src/xenia/kernel/xam/apps/xgi_app.cc
+++ src/xenia/kernel/xam/apps/xgi_app.cc
@@ -359,7 +359,43 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
             stat[statIndex].Value.type = static_cast<X_USER_DATA_TYPE>(
                 (*statObjectPtr)["type"].GetUint());
 
-            switch (stat[statIndex].Value.type) {
+            X_USER_DATA_TYPE stat_type = stat[statIndex].Value.type;
+
+            switch (stat_type) {
+              case X_USER_DATA_TYPE::CONTEXT: {
+                XELOGW("Statistic type: CONTEXT");
+              } break;
+              case X_USER_DATA_TYPE::INT32: {
+                XELOGW("Statistic type: INT32");
+              } break;
+              case X_USER_DATA_TYPE::INT64: {
+                XELOGW("Statistic type: INT64");
+              } break;
+              case X_USER_DATA_TYPE::DOUBLE: {
+                XELOGW("Statistic type: DOUBLE");
+              } break;
+              case X_USER_DATA_TYPE::WSTRING: {
+                XELOGW("Statistic type: WSTRING");
+              } break;
+              case X_USER_DATA_TYPE::FLOAT: {
+                XELOGW("Statistic type: FLOAT");
+              } break;
+              case X_USER_DATA_TYPE::BINARY: {
+                XELOGW("Statistic type: BINARY");
+              } break;
+              case X_USER_DATA_TYPE::DATETIME: {
+                XELOGW("Statistic type: DATETIME");
+              } break;
+              case X_USER_DATA_TYPE::UNSET: {
+                XELOGW("Statistic type: UNSET");
+              } break;
+              default:
+                XELOGW("Unsupported statistic type.",
+                       static_cast<uint32_t>(stat_type));
+                break;
+            }
+
+            switch (stat_type) {
               case X_USER_DATA_TYPE::INT32:
                 stat[statIndex].Value.data.u32 =
                     (*statObjectPtr)["value"].GetUint();
@@ -379,6 +415,7 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
 
             stat[statIndex].Value.type = static_cast<X_USER_DATA_TYPE>(
                 (*statObjectPtr)["type"].GetUint());
+
             statIndex++;
           }
 
