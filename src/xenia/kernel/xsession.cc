661151c7104cad44ba2e6dbddb7a29fdf41af6a2
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-11-05 19:36:46 +0000

    [XAM] Replace user index defines with constants

--- src/xenia/kernel/xsession.cc
+++ src/xenia/kernel/xsession.cc
@@ -71,7 +71,7 @@ X_RESULT XSession::CreateSession(uint8_t user_index, uint8_t public_slots,
   uint64_t* Nonce_ptr =
       kernel_state_->memory()->TranslateVirtual<uint64_t*>(nonce_ptr);
 
-  local_details_.UserIndexHost = X_USER_INDEX_NONE;
+  local_details_.UserIndexHost = XUserIndexNone;
 
   // CSGO only uses STATS flag to create a session to POST stats pre round.
   // Minecraft and Portal 2 use flags HOST + STATS.
@@ -290,11 +290,11 @@ X_RESULT XSession::JoinSession(XSessionJoin* data) {
       member->UserIndex = user_index;
 
       local_details_.ActualMemberCount = std::min<int32_t>(
-          X_USER_MAX_USERS, local_details_.ActualMemberCount + 1);
+          XUserMaxUserCount, local_details_.ActualMemberCount + 1);
     } else {
       // Default member
       const xe::be<uint64_t> xuid = xuid_array[i];
-      const uint32_t user_index = X_USER_INDEX_NONE;
+      const uint32_t user_index = XUserIndexNone;
 
       const bool is_member_added =
           remote_members_.find(xuid) != remote_members_.end();
@@ -315,7 +315,7 @@ X_RESULT XSession::JoinSession(XSessionJoin* data) {
             profile_manager->GetUserIndexAssignedToProfile(xuid);
 
         local_details_.ActualMemberCount = std::min<int32_t>(
-            X_USER_MAX_USERS, local_details_.ActualMemberCount + 1);
+            XUserMaxUserCount, local_details_.ActualMemberCount + 1);
       }
     }
 
@@ -733,7 +733,7 @@ X_RESULT XSession::GetSessionByID(Memory* memory,
   }
 
   if (search_data->user_index < 0 ||
-      search_data->user_index >= X_USER_MAX_USERS) {
+      search_data->user_index >= XUserMaxUserCount) {
     return X_ERROR_INVALID_PARAMETER;
   }
 
@@ -755,7 +755,7 @@ X_RESULT XSession::GetSessionByIDs(Memory* memory,
   }
 
   if (search_data->user_index < 0 ||
-      search_data->user_index >= X_USER_MAX_USERS) {
+      search_data->user_index >= XUserMaxUserCount) {
     return X_ERROR_INVALID_PARAMETER;
   }
 
