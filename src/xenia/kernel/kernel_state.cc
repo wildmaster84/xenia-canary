734d1013b9d7a366a6afacb13e908b3ef0d2a3b9
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-11-06 11:41:56 +0000

    [Kernel] Network mode selector

--- src/xenia/kernel/kernel_state.cc
+++ src/xenia/kernel/kernel_state.cc
@@ -41,7 +41,7 @@ DEFINE_uint32(kernel_build_version, 1888, "Define current kernel version",
 
 DECLARE_string(cl);
 
-DECLARE_bool(offline_mode);
+DECLARE_int32(network_mode);
 
 namespace xe {
 namespace kernel {
@@ -1045,10 +1045,13 @@ void KernelState::RegisterNotifyListener(XNotifyListener* listener) {
   if (!has_notified_live_startup_ && listener->mask() & kXNotifyLive) {
     has_notified_live_startup_ = true;
 
-    uint32_t live_connection_state =
-        cvars::offline_mode ? X_ONLINE_S_LOGON_DISCONNECTED
-                            : X_ONLINE_S_LOGON_CONNECTION_ESTABLISHED;
-    uint32_t ethernet_link_state = cvars::offline_mode ? 0 : 1;
+    const uint32_t live_connection_state =
+        cvars::network_mode == NETWORK_MODE::XBOXLIVE
+            ? X_ONLINE_S_LOGON_CONNECTION_ESTABLISHED
+            : X_ONLINE_S_LOGON_DISCONNECTED;
+
+    const uint32_t ethernet_link_state =
+        cvars::network_mode == NETWORK_MODE::OFFLINE ? 0 : 1;
 
     listener->EnqueueNotification(kXNotificationLiveConnectionChanged,
                                   live_connection_state);
--- src/xenia/kernel/xam/xam_net.cc
+++ src/xenia/kernel/xam/xam_net.cc
@@ -42,7 +42,7 @@ DECLARE_bool(logging);
 
 DECLARE_bool(log_mask_ips);
 
-DECLARE_bool(offline_mode);
+DECLARE_int32(network_mode);
 
 DECLARE_bool(xlink_kai_systemlink_hack);
 
@@ -627,11 +627,16 @@ dword_result_t NetDll_XNetGetTitleXnAddr_entry(dword_t caller,
     return XnAddrStatus::XNET_GET_XNADDR_PENDING;
   }
 
-  uint8_t status = XnAddrStatus::XNET_GET_XNADDR_STATIC |
-                   XnAddrStatus::XNET_GET_XNADDR_GATEWAY |
-                   XnAddrStatus::XNET_GET_XNADDR_DNS;
+  uint8_t status = 0;
 
-  if (XLiveAPI::IsOnline()) {
+  if (cvars::network_mode != NETWORK_MODE::OFFLINE) {
+    status |= XnAddrStatus::XNET_GET_XNADDR_ETHERNET |
+              XnAddrStatus::XNET_GET_XNADDR_STATIC |
+              XnAddrStatus::XNET_GET_XNADDR_GATEWAY |
+              XnAddrStatus::XNET_GET_XNADDR_DNS;
+  }
+
+  if (cvars::network_mode == NETWORK_MODE::XBOXLIVE) {
     status |= XnAddrStatus::XNET_GET_XNADDR_ONLINE;
   }
 
@@ -782,7 +787,7 @@ dword_result_t NetDll_XNetXnAddrToInAddr_entry(dword_t caller,
     in_addr->s_addr = xn_addr->ina.s_addr;
   }
 
-  if (XLiveAPI::IsOnline()) {
+  if (XLiveAPI::IsConnectedToServer()) {
     in_addr->s_addr = xn_addr->inaOnline.s_addr;
   }
 
@@ -906,7 +911,7 @@ dword_result_t NetDll_XNetGetBroadcastVersionStatus_entry(dword_t caller,
 DECLARE_XAM_EXPORT1(NetDll_XNetGetBroadcastVersionStatus, kNetworking, kStub);
 
 dword_result_t NetDll_XNetGetEthernetLinkStatus_entry(dword_t caller) {
-  if (cvars::offline_mode) {
+  if (cvars::network_mode == NETWORK_MODE::OFFLINE) {
     return 0;
   }
 
