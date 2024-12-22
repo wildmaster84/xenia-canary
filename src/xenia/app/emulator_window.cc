5d107e59827df2bf4043977e90622d94d1be085c
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-12-22 11:58:37 +0000

    [APP] Add host notification for network mode

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -1481,6 +1481,8 @@ void EmulatorWindow::SetNetworkMode(int32_t mode) {
     return;
   }
 
+  std::string mode_desc = "";
+
   switch (mode) {
     case xe::kernel::NETWORK_MODE::OFFLINE: {
       emulator_->kernel_state()->BroadcastNotification(
@@ -1489,7 +1491,7 @@ void EmulatorWindow::SetNetworkMode(int32_t mode) {
       emulator_->kernel_state()->BroadcastNotification(
           kXNotificationIDLiveLinkStateChanged, 0);
 
-      XELOGI("Swtiched Network Mode: Offline");
+      mode_desc = "Offline";
     } break;
     case xe::kernel::NETWORK_MODE::LAN: {
       emulator_->kernel_state()->BroadcastNotification(
@@ -1498,7 +1500,7 @@ void EmulatorWindow::SetNetworkMode(int32_t mode) {
       emulator_->kernel_state()->BroadcastNotification(
           kXNotificationIDLiveLinkStateChanged, 1);
 
-      XELOGI("Swtiched Network Mode: LAN/Systemlink");
+      mode_desc = "LAN/Systemlink";
     } break;
     case xe::kernel::NETWORK_MODE::XBOXLIVE: {
       emulator_->kernel_state()->BroadcastNotification(
@@ -1508,9 +1510,16 @@ void EmulatorWindow::SetNetworkMode(int32_t mode) {
       emulator_->kernel_state()->BroadcastNotification(
           kXNotificationIDLiveLinkStateChanged, 1);
 
-      XELOGI("Swtiched Network Mode: Xbox Live");
+      mode_desc = "Xbox Live";
     } break;
   }
+
+  app_context_.CallInUIThread([&]() {
+    new xe::ui::HostNotificationWindow(imgui_drawer(), "Network Mode",
+                                       mode_desc, 0);
+  });
+
+  XELOGI("Swtiched Network Mode: {}", mode_desc);
 
   xe::kernel::XLiveAPI::SetNetworkMode(mode);
 }
