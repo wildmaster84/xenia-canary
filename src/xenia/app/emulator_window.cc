f284fbaa105190a13fbb0f376e8ad1ca7dedf1bf
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2025-11-08 00:42:08 +0000

    [APP] Use this repo in links

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -1063,7 +1063,7 @@ bool EmulatorWindow::Initialize() {
     help_menu->AddChild(MenuItem::Create(
         MenuItem::Type::kString, "Recent changes on GitHub...", []() {
           LaunchWebBrowser(
-              "https://github.com/xenia-canary/xenia-canary/"
+              "https://github.com/AdrianCassar/xenia-canary/"
               "compare/" XE_BUILD_COMMIT "..." XE_BUILD_BRANCH);
         }));
     help_menu->AddChild(MenuItem::Create(MenuItem::Type::kSeparator));
@@ -2086,10 +2086,10 @@ void EmulatorWindow::ShowFAQ() {
 void EmulatorWindow::ShowBuildCommit() {
 #ifdef XE_BUILD_IS_PR
   LaunchWebBrowser(
-      "https://github.com/xenia-canary/xenia-canary/pull/" XE_BUILD_PR_NUMBER);
+      "https://github.com/AdrianCassar/xenia-canary/pull/" XE_BUILD_PR_NUMBER);
 #else
   LaunchWebBrowser(
-      "https://github.com/xenia-canary/xenia-canary/commit/" XE_BUILD_COMMIT);
+      "https://github.com/AdrianCassar/xenia-canary/commit/" XE_BUILD_COMMIT);
 #endif
 }
 
