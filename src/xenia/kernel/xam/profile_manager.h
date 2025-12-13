701cb216e80bce8d91590e92c4e6c8a7ba490c06
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2025-12-13 21:16:22 +0000

    [XAM] Fixed SignedInProfilesCount util function

--- src/xenia/kernel/xam/profile_manager.h
+++ src/xenia/kernel/xam/profile_manager.h
@@ -120,7 +120,9 @@ class ProfileManager {
   bool IsAnyProfileSlotFree() const {
     return logged_profiles_.size() < XUserMaxUserCount;
   }
-  uint32_t SignedInProfilesCount() const { return !logged_profiles_.size(); }
+  uint32_t SignedInProfilesCount() const {
+    return static_cast<uint32_t>(logged_profiles_.size());
+  }
 
   std::filesystem::path GetProfileContentPath(
       const uint64_t xuid, const uint32_t title_id = -1,
