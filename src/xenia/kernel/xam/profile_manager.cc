58cadb5a27e199869601edd10788e2645ac090a1
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2026-06-28 22:59:46 +0100

    [XAM] Set default Xbox Live account settings during conversion

--- src/xenia/kernel/xam/profile_manager.cc
+++ src/xenia/kernel/xam/profile_manager.cc
@@ -622,6 +622,8 @@ void ProfileManager::SetDefaultXboxLiveEnabledAccountSettings(
           XCONFIG_USER_CATEGORY,
           XCONFIG_USER_CATEGORY_ENTRIES::XCONFIG_USER_LANGUAGE));
 
+  account.ToggleLiveFlag(true);
+
   account.SetCountry(country_id);
   account.SetLanguage(desired_language);
 
@@ -752,11 +754,10 @@ bool ProfileManager::ConvertToXboxLiveEnabledProfile(const uint64_t xuid) {
   user_tracker_->SetupDefaultProfileSettings(xuid);
 
   auto run = [this](X_XAMACCOUNTINFO& account_info) {
-    account_info.ToggleLiveFlag(true);
+    SetDefaultXboxLiveEnabledAccountSettings(account_info);
 
     // Set default settings and online XUID once
     if (!account_info.xuid_online) {
-      SetDefaultXboxLiveEnabledAccountSettings(account_info);
       account_info.xuid_online = GenerateXuidOnline();
     }
 
